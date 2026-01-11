8a61ca9ce46b5ef0577f98220ed745517100a704
Yuliia <semockoula@gmail.com>
2026-01-11 21:53:43 +0200

    add env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://notehub-public.goit.study/api',
-  token: 'Token',
+  token:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNlbW9ja291bGFAZ21haWwuY29tIiwiaWF0IjoxNzY2MjI3NDAwfQ.vgBCzBrToVspBfk6cBhv-xb5OwvIXzZCRbZFwWXEbiM',
 };
